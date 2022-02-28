01fa9e769659662719ae6032ee9c9e864623c8c9
Salmansoori <salmansoori509@gmail.com>
2022-02-28 12:01:27 +0530

    Added Structured JS code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-menuSection = [
+/* menuSection = [
     {
         id:"recommended",
         name:"recommended",
@@ -82,24 +82,20 @@ menuSection = [
     },
     
 ];
+*/
 
 
-
-let recommendedList = document.getElementById("rec");
-let menulist = document.getElementById("menu")
-
-function createSectionElementForRecList(section){
+function createLeftMenuSection(section){
     const a = document.createElement('a');
     const text = document.createTextNode(section.name);
     a.appendChild(text);
-    a.href = "#"+section.id;
-    const newRecItem = document.createElement('li');
-    newRecItem.appendChild(a);
-    //console.log(newRecItem);
-    recommendedList.appendChild(newRecItem)
-    const menuItem = document.createElement('div');
+    a.href = "#" + section.id;
+    const newLeftMenuItem = document.createElement('li');
+    newLeftMenuItem.appendChild(a);
+    return newLeftMenuItem;    
+}
 
-    const menuItemChild = document.createElement('div');
+function createMenuListItemHead(section){
     const menuItemChildHead = document.createElement('div');
     const itemHead = document.createElement('h1');
     const itemHeadText = document.createTextNode(section.name);
@@ -109,65 +105,124 @@ function createSectionElementForRecList(section){
     itemCount.appendChild(itemCountText);
     menuItemChildHead.appendChild(itemHead);
     menuItemChildHead.appendChild(itemCount);
-    menuItemChild.appendChild(menuItemChildHead);
-    //console.log(menuItemChild);
-    menuItemChild.classList.add("item-head");
-    menuItemChild.setAttribute('id',section.id)
-    for( let itemInd=0; itemInd< section.menulist.length; itemInd++){
-    
+    return menuItemChildHead;
+}
+
+function createDishitemName(name){
+    const dishItemName = document.createElement('div');
+    dishItemName.classList.add("item-name")
+    const dishItemNameText = document.createTextNode(name);
+    dishItemName.appendChild(dishItemNameText);
+    return dishItemName;
+}
+
+function createDishItemCost(cost){
+    const dishItemCost = document.createElement('div');
+    dishItemCost.classList.add("item-price")
+    const dishItemCostText = document.createTextNode(cost);
+    dishItemCost.appendChild(dishItemCostText);
+    return dishItemCost;
+}
+
+function createDishItemDesc(desc){
+    const dishItemDesc = document.createElement('div');
+    dishItemDesc.classList.add("item-desc")
+    const dishItemDescText = document.createTextNode(desc);
+    dishItemDesc.appendChild(dishItemDescText);
+    return dishItemDesc;
+}
+
+function createDishItemPic(img){
+    const dishItemPicImg = document.createElement('div');
+    const image = document.createElement('img');
+    image.src = img
+    image.width = 140;
+    dishItemPicImg.appendChild(image);
+    return dishItemPicImg;
+}
+
+function createDishItemPicButton(){
+    const dishItemPicAddB = document.createElement('div');
+    dishItemPicAddB.classList.add("item-add")
+    const button = document.createElement('button');
+    const buttonText = document.createTextNode("Add");
+    button.appendChild(buttonText);
+    dishItemPicAddB.appendChild(button)
+    return dishItemPicAddB;
+}
+
+function createNewDish(dish){
     const newDish = document.createElement('div');
 
     const newDishItem = document.createElement('div');
     newDishItem.classList.add("item")
 
-    const newDishItemName = document.createElement('div');
-    newDishItemName.classList.add("item-name")
-    const newDishItemNameText = document.createTextNode(section.menulist[itemInd].name);
-    newDishItemName.appendChild(newDishItemNameText);
+    // dish name div
+    const newDishItemName = createDishitemName(dish.name); 
     newDishItem.appendChild(newDishItemName);
-
-    const newDishItemCost = document.createElement('div');
-    newDishItemCost.classList.add("item-price")
-    const newDishItemCostText = document.createTextNode(section.menulist[itemInd].cost);
-    newDishItemCost.appendChild(newDishItemCostText);
+    
+    //dish cost div
+    const newDishItemCost = createDishItemCost(dish.cost);
     newDishItem.appendChild(newDishItemCost);
 
-    const newDishItemDesc = document.createElement('div');
-    newDishItemDesc.classList.add("item-desc")
-    const newDishItemDescText = document.createTextNode(section.menulist[itemInd].desc);
-    newDishItemDesc.appendChild(newDishItemDescText);
+    //dish description div
+    const newDishItemDesc = createDishItemDesc(dish.desc);
     newDishItem.appendChild(newDishItemDesc);
-    newDish.appendChild(newDishItem)
 
+    newDish.appendChild(newDishItem)
+    
+    // dish pic div
     const newDishItemPic = document.createElement('div');
     newDishItemPic.classList.add("item-pic")
 
-    const newDishItemPicImg = document.createElement('div');
-    const image = document.createElement('img');
-    image.src = section.menulist[itemInd].img
-    image.width=140;
-    newDishItemPicImg.appendChild(image)
+    const newDishItemPicImg = createDishItemPic(dish.img);
     newDishItemPic.appendChild(newDishItemPicImg)
-
-    const newDishItemPicAddB = document.createElement('div');
-    newDishItemPicAddB.classList.add("item-add")
-    const button = document.createElement('button');
-    const buttonText = document.createTextNode("Add");
-    button.appendChild(buttonText);
-    newDishItemPicAddB.appendChild(button);
+    
+    // dish button div
+    const newDishItemPicAddB = createDishItemPicButton();
     newDishItemPic.appendChild(newDishItemPicAddB);
+
     newDish.appendChild(newDishItemPic)
-    newDish.classList.add("dish")
+    newDish.classList.add("dish");
+    return newDish;
+}
 
-    menuItemChild.appendChild(newDish)
+function createSectionElementForMenu(section) {
+
+    //left Menu Item
+    let recommendedList = document.getElementById("rec");
+    const leftMenuItem = createLeftMenuSection(section);
+    recommendedList.appendChild(leftMenuItem)
+
+    //right menu item list
+    const menuItemChild = document.createElement('div');
+    const menuListItemChildHead = createMenuListItemHead(section); 
+    menuItemChild.appendChild(menuListItemChildHead);
+    //console.log(menuItemChild);
+    menuItemChild.classList.add("item-head");
+    menuItemChild.setAttribute('id', section.id)
+    
+    // add dishes
+    for (let itemInd = 0; itemInd < section.menulist.length; itemInd++) {
+        const newDish = createNewDish(section.menulist[itemInd]);
+        menuItemChild.appendChild(newDish)
     };
-    //console.log(menulist)
-    menulist.appendChild(menuItemChild)
 
+    return menuItemChild;
 }
 
-for(let ind=0; ind<menuSection.length; ind++){
-    const section = menuSection[ind];
-    //console.log(section);
-    createSectionElementForRecList(section);
-}
+fetch('./restaurant_data.json')
+    .then(res => res.json())
+    .then((data) => {
+        //console.log(data);
+        let menuSection = data.menuSection;
+        let menulist = document.getElementById('menu')
+        for (let ind = 0; ind < menuSection.length; ind++) {
+            const section = menuSection[ind];
+            //console.log(section);
+            const sectionElement = createSectionElementForMenu(section);
+            menulist.appendChild(sectionElement);
+        }
+    }).catch(err => {
+        console.log(err);
+    })
